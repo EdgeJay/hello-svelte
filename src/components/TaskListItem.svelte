15d271582e2b24396fbc1ee0349ec8e7a6efc164
Wu Huijie <wu_huijie@tech.gov.sg>
2024-11-20 01:11:01 +0800

    add ui to edit, save and remove task item

--- src/components/TaskListItem.svelte
+++ src/components/TaskListItem.svelte
@@ -1,13 +1,61 @@
 <script lang="ts">
+  import { app } from '@/store.svelte';
   import { Task } from '@/models/tasks';
 
   interface Props {
     task: Task;
+    editMode: boolean;
   }
 
-  let { task }: Props = $props();
+  let { task, editMode }: Props = $props();
+  let checked = $state(task.done);
+  let editedDescription = $state(task.description);
+  let isEditing = $state(editMode);
+
+  const onDoneChange = () => {
+    task.toggleDone();
+  };
+
+  const onEdit = () => {
+    isEditing = true;
+  };
+
+  const onSave = () => {
+    isEditing = false;
+    task.description = editedDescription;
+  };
+
+  const onRemove = () => {
+    app.tasks = app.tasks.reduce((acc, t) => {
+      if (t.id !== task.id) {
+        acc.push(t);
+      }
+      return acc;
+    }, [] as Task[]);
+  };
 </script>
 
 <div>
-  <p>{task.description}</p>
+  <label class="checkbox">
+    <input type="checkbox" onchange={onDoneChange} bind:checked />
+    {#if !isEditing}
+      {#if checked}
+        <s>{task.description}</s>
+      {:else}
+        {task.description}
+      {/if}
+    {:else}
+      <input
+        type="text"
+        placeholder="Task description here"
+        bind:value={editedDescription}
+      />
+    {/if}
+  </label>
+  {#if !isEditing}
+    <button onclick={onEdit}>Edit</button>
+  {:else}
+    <button onclick={onSave}>Save</button>
+  {/if}
+  <button onclick={onRemove}>Remove</button>
 </div>
