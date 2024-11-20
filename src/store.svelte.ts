da140d6f9dd4cc180dc504ede3d062bdf8a618d2
Wu Huijie <wu_huijie@tech.gov.sg>
2024-11-21 01:51:44 +0800

    add functions to manipulate tasks

--- src/store.svelte.ts
+++ src/store.svelte.ts
@@ -2,8 +2,28 @@ import { Task } from '@/models/tasks';
 
 interface AppStore {
   tasks: Task[];
+  completedCount: number;
 }
 
 export const app: AppStore = $state({
   tasks: [],
+  completedCount: 0,
 });
+
+export const refreshCompletedCount = () => {
+  app.completedCount = app.tasks.reduce((acc, task) => {
+    if (task.done) {
+      acc += 1;
+    }
+    return acc;
+  }, 0);
+};
+
+export const removeTask = (taskId: string) => {
+  app.tasks = app.tasks.reduce((acc, t) => {
+    if (t.id !== taskId) {
+      acc.push(t);
+    }
+    return acc;
+  }, [] as Task[]);
+};
